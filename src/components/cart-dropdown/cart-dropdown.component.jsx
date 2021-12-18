9b07bc5cee21d5b8e7597b000aca97563af987ed
Lazar Dilov <ldilov@isimarkets.com>
2021-12-18 09:08:22 +0200

    Added cart-item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,25 @@
+import { connect } from 'react-redux';
+
+import CartItemComponent from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
+  const items = cartItems.map((item) => <CartItemComponent item={item} key={item.id} />);
+
   return (
     <div className='cart-dropdown'>
-      <div className='cart-items'>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p> Dummy</p>
-        <p>Test</p>
-        <h1>Test</h1>
-      </div>
+      <div className='cart-items'>{items}</div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state.cart.cartItems
+  };
+};
+
+export default connect(mapStateToProps, null)(CartDropdown);
