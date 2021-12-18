723567a4315249ad372f52910604097b9d41a0a4
Lazar Dilov <ldilov@isimarkets.com>
2021-12-18 09:48:41 +0200

    Fixed cart number icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,19 @@
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-import { connect } from 'react-redux';
+import { connect, useSelector } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-icon.styles.scss';
 
 const CartIcon = ({ toggleCartHidden }) => {
+  const cartItems = useSelector((store) => store.cart.cartItems);
+  const cartQuantity =
+    cartItems.length > 0 ? cartItems.reduce((acc, item) => acc + item.quantity, 0) : 0;
+
   return (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartQuantity}</span>
     </div>
   );
 };
